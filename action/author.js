19fd5f98b3de24d6c9d81e767ceea0125dcb142b
lansu <intelliasds@gmail.com>
2019-08-23 13:26:59 +0300

    init README

---
+++ action/author.js
@@ -0,0 +1,8 @@
+const CustomError = require('component/customError.js');
+const Config = require('../config');
+const query = require('../db/query');
+const { db } = new Config();
+
+module.exports = {
+	checkExist: ()
+};
--- db/schema/author.js
+++ db/schema/author.js
@@ -2,5 +2,6 @@ const defaultFieldList = require('../defaultFieldList');
 
 module.exports = (name) => `CREATE TABLE ${name} (
 name VARCHAR(255) NOT NULL,
+UNIQUE KEY name (name),
 ${defaultFieldList}
 )`;
