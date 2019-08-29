e7b8d2c8dd4a5c6433847263df138b22f11c2417
lansu <intelliasds@gmail.com>
2019-08-29 10:40:22 +0300

    fix update book 'updated'

--- db/trigger/index.js
+++ db/trigger/index.js
@@ -2,6 +2,6 @@ module.exports = [
 	`CREATE TRIGGER when_update_author_book_set_book_update_time AFTER UPDATE ON author_book
 	 FOR EACH ROW
 	 UPDATE book
-		 SET updated = CURRENT_TIMESTAMP
-		 WHERE id = NEW.book_id;`
+		 SET book.updated = CURRENT_TIMESTAMP
+		 WHERE book.id = NEW.book_id;`
 ];
