e6a3ceb7a6d84d24f2ffbc6dfe9d79db6a1ac18d
lansu <intelliasds@gmail.com>
2019-09-03 11:51:30 +0300

    book validator 'get'

--- tests/validator/book.spec.js
+++ tests/validator/book.spec.js
@@ -11,26 +11,57 @@ const validData = {
 	sort: "title:asc",
 };
 
-const getAllowFieldList = ['filter', 'page', 'size'];
+const getAllowFieldList = ['sort', 'filter', 'page', 'size'];
 
-describe('Validate image', () => {
+describe('Validate book', () => {
 
 	describe('get', () => {
-		it('Right work \'get\'', () => {
+		it('Right work', () => {
 			const result = validator.get(validData);
 
 			expect(result).to.be.an('object');
 			expect(result).to.deep.equal(validData);
 		});
 
-		it('Return only allowed fields in \'get\'', async () => {
+		it('Return only allowed fields', () => {
 			const extendObject = { some: 'field', ...validData };
-			const result = await validator.get(validData);
+			const result = validator.get(extendObject);
 
-			// expect(result).to.be.an('object');
-			// expect(result).to.have.all.keys(getAllowFieldList);
-			// expect(result).to.deep.equal(validData);
+			expect(result).to.be.an('object');
+			expect(result).to.have.all.keys(getAllowFieldList);
+			expect(result).to.deep.equal(validData);
 		});
+
+		it('Error if wrong sort', () => {
+			try {
+				validator.get({ ...validData, ...{ sort: 'name:up' } });
+			} catch (err) {
+				expect(err).to.be.instanceOf(CustomError);
+				expect(err).to.have.all.keys('message', 'status', 'code');
+				expect(err.message).to.deep.equal([
+					{
+						actual: undefined,
+						type: 'string',
+						expected: /^((id|title|author|image):(asc|desc)(,s?)?)*$/g,
+						field: 'sort',
+						message: 'The \'sort\' field fails to match the required pattern!',
+						type: 'stringPattern',
+					}
+				]);
+				expect(err.status).to.equal(400);
+				expect(err.code).to.equal(1);			}
+		});
+
+		it('Request data sould be object', () => {
+			try {
+				validator.get('data as string');
+			} catch (err) {
+				expect(err).to.be.instanceOf(CustomError);
+				expect(err).to.have.all.keys('message', 'status', 'code');
+				expect(err.message).to.deep.equal('Input data should be object');
+				expect(err.status).to.equal(400);
+				expect(err.code).to.equal(1);			}
+		})
 	});
 
 });
--- validator/book.js
+++ validator/book.js
@@ -24,7 +24,7 @@ const schemaUpdate = {
 };
 
 const schemaRead = {
-	sort: { type: "string", min: 3, max: 255, pattern: config.regex.sortBookInRequest, optional: true },
+	sort: { type: "string", min: 3, max: 255, pattern: config.regexAsString.sortBookInRequest, patternFlags: 'g', optional: true },
 	filter: { type: "string", min: 1, max: 1000, optional: true },
 	size: { type: "number", positive: true, integer: true, optional: true, convert: true },
 	page: { type: "number", positive: true, integer: true, optional: true, convert: true },
